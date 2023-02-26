4aa8e6ce2a1eb017586435d41764b77a1168351c
itsnewray <111432109+itsnewray@users.noreply.github.com>
2023-02-26 22:29:00 +0600

    lab6

---
+++ main.js
@@ -0,0 +1,15 @@
+function checkPositive (number)
+{
+    if (number < 0) {
+        console.log("number is negative")
+    }
+    else if (number > 0) {
+        console.log("number is positive")
+    }
+    else {
+        console.log("number is equal to zero")
+    }
+}
+checkPositive (-50)
+checkPositive (50)
+checkPositive (4)
